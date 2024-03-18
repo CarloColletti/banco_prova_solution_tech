ed23caf80f385206c1bbaf4c951dc8ed30f37da8
CarloColletti~ <carlocolletti5@gmail.com>
2024-03-18 17:34:47 +0100

    user online hasn't in the list

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Home extends Controller
 {
     public function home()
     {
-        $users = User::all();
+        // $users = User::all();
+        $users = User::all()->except(Auth::id());
 
         return view('home', compact('users'));
     }
