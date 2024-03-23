8bd5bdaeae01ffc00322d38c7cf1e59021947565
CarloColletti~ <carlocolletti5@gmail.com>
2024-03-23 22:11:00 +0100

    add base to soft delete

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,17 +16,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            // $table->string('lastname');
-            // $table->string('fiscal_code');
-            // $table->string('address');
-            // $table->string('province');
-            // $table->string('city');
-            // $table->string('country');
-            // $table->string('zip_code');
-            // $table->string('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
