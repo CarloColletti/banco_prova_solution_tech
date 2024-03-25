ba9ccfe358a6366da41f447bf74f34c1a730c58a
CarloColletti~ <carlocolletti5@gmail.com>
2024-03-25 10:43:20 +0100

    pruduct web.php update

--- Modules/Product/Routes/web.php
+++ Modules/Product/Routes/web.php
@@ -11,6 +11,21 @@
 |
 */
 
-Route::prefix('product')->group(function() {
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('product')->group(function () {
     Route::get('/', 'ProductController@index');
+});
+
+Route::prefix('product')->group(function () {
+    Route::get('/', 'ProductController@index')->name('product.index');
+    Route::post('/', 'ProductController@store')->name('product.store');
+    Route::get('/create', 'ProductController@create')->name('product.create');
+    Route::get('/{product}', 'ProductController@show')->name('product.show');
+    Route::match(['put', 'patch'], '/{user}', 'ProductController@update')->name('product.update');
+    Route::delete('/{product}', 'ProductController@destroy')->name('product.destroy');
+    Route::get('/{product}/edit', 'ProductController@edit')->name('product.edit');
+    Route::get('/product/trash', 'ProductController@trash')->name('product.trash');
+    Route::post('/product/{product}/restore', 'ProductController@restore')->name('product.restore');
+    Route::delete('/product/{product}/force-delete', 'ProductController@force_delete')->name('product.force_delete');
 });
