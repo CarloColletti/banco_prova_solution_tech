4cd5869c07fb728478e66988d6b3e39d563674f4
CarloColletti~ <carlocolletti5@gmail.com>
2024-03-25 17:16:42 +0100

    modal for create (to complete)

--- Modules/Product/Resources/views/index.blade.php
+++ Modules/Product/Resources/views/index.blade.php
@@ -6,6 +6,12 @@
 
 @section('content')
     <div class=" d-flex flex-row  justify-content-between pb-4">
+        <div>
+            <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#createProduct">
+                Nuovo Prodotto
+            </button>
+        </div>
+
 
         {{-- link to product trashcan  --}}
         <a href="{{ route('product.trash') }}">
@@ -50,6 +56,11 @@
 
                         <div>
                             <i class="fa-regular fa-eye"></i>
+
+                            <button type="button" class="btn btn-sm btn-primary" data-product-id="{{ $product->id }}"
+                                data-product-name="{{ $product->name }}">
+                                <i class="fa-regular fa-eye"></i> Apri modal
+                            </button>
                         </div>
 
                         <div>
@@ -62,8 +73,6 @@
                             <i class="fa-regular fa-trash-can"></i>
 
                         </div>
-
-
                     </th>
                 </tr>
             @endforeach
@@ -76,8 +85,143 @@
 @section('modal')
     {{-- modal for detail --}}
 
+    <div id="modal" class="modal hidden">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Dati prodotto</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>ID Prodotto: <span id="modal-product-id"></span></p>
+                    <p>Nome Prodotto: <span id="modal-product-name"></span></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     {{-- end modal for detail --}}
 
+
+    {{-- Modal for create --}}
+    <div class="modal fade" id="createProduct" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Crea Prodotto</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="{{ route('product.store') }}" enctype="multipart/form-data" method="POST"
+                    class="form-create" data-modalita="create">
+                    <div class="modal-body">
+                        @csrf
+                        {{-- name --}}
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Nome:</label>
+                            <input name="text" class="form-control" id="name" name="name"
+                                value="{{ old('name') }}">
+                            @error('name')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        {{-- type --}}
+                        <div class="mb-3">
+                            <label for="type" class="form-label">Tipologia:</label>
+                            <input type="text" class="form-control" id="type" name="type"
+                                value="{{ old('type') }}">
+                            @error('type')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        {{-- weight --}}
+                        <div class="mb-3">
+                            <label for="weight" class="form-label">Peso:</label>
+                            <input type="text" class="form-control" id="weight" name="weight"
+                                value="{{ old('weight') }}">
+                            @error('weight')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        {{-- height --}}
+                        <div class="mb-3">
+                            <label for="height" class="form-label">Altezza:</label>
+                            <input type="text" class="form-control" id="height" name="height"
+                                value="{{ old('height') }}">
+                            @error('height')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        {{-- width --}}
+                        <div class="mb-3">
+                            <label for="width" class="form-label">Larghezza:</label>
+                            <input type="text" class="form-control" id="width" name="width"
+                                value="{{ old('width') }}">
+                            @error('width')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        {{-- depth --}}
+                        <div class="mb-3">
+                            <label for="depth" class="form-label">Profondità:</label>
+                            <input type="text" class="form-control" id="depth" name="depth"
+                                value="{{ old('depth') }}">
+                            @error('depth')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+
+                        {{-- stock_quntity --}}
+                        <div class="mb-3">
+                            <label for="stock_quntity" class="form-label">Quantità:</label>
+                            <input type="number" class="form-control" id="stock_quntity" name="stock_quntity"
+                                value="{{ old('stock_quntity') }}">
+                            @error('stock_quntity')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        {{-- price --}}
+                        <div class="mb-3">
+                            <label for="price" class="form-label">Prezzo:</label>
+                            <input type="number" step=".01" class="form-control" id="price" name="price"
+                                value="{{ old('price') }}">
+                            @error('price')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+
+                        {{-- IMAGES INPUT --}}
+                        <div class="mb-4">
+                            <div class="form-group py-3 d-flex flex-column">
+                                <label for="photo_link">Scegli foto:</label>
+                                <input class="py-2" type="file" class="form-control" id="photo_link"
+                                    name="photo_link" @if (empty($product->product_image)) required @endif>
+                                <div id="image-validation-message"></div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+    {{-- End Modal for create --}}
 
     {{-- modal for delete --}}
 
