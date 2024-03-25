cd28295b6f841d4fca2385ace9814cbce5875451
CarloColletti~ <carlocolletti5@gmail.com>
2024-03-25 10:44:41 +0100

    composer i and npm i on product

---
+++ Modules/Product/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nwidart/product',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5c8b77fdcbf0c9ccfa2ed37cfe04f8f82543409c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nwidart/product' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5c8b77fdcbf0c9ccfa2ed37cfe04f8f82543409c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
