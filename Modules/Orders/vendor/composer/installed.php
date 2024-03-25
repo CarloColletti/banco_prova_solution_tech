ce8cefac47f4984e00787aacfc1ab75d1086aad4
CarloColletti~ <carlocolletti5@gmail.com>
2024-03-25 10:03:27 +0100

    run composer i and npm i

---
+++ Modules/Orders/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nwidart/orders',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '343504823654f1fa23e6f7720a0723dd3620b8a8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nwidart/orders' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '343504823654f1fa23e6f7720a0723dd3620b8a8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
