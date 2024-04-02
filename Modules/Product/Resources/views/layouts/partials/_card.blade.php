fd4952b614072fa3d14fd0cc0a3caa1518057817
CarloColletti~ <carlocolletti5@gmail.com>
2024-04-02 09:41:22 +0200

    fix style align

--- Modules/Product/Resources/views/layouts/partials/_card.blade.php
+++ Modules/Product/Resources/views/layouts/partials/_card.blade.php
@@ -9,7 +9,7 @@
          <li class="list-group-item">Prezzo: {{ $product->price }}€</li>
          <li class="list-group-item">Tipo: {{ $product->type }}</li>
      </ul>
-     <div class="card-body d-flex gap-4">
+     <div class="card-body d-flex align-items-center gap-4">
          <button type="button" class="btn btm-show text-primary p-0 m-0"
              data-product-stock="{{ $product->stock_quntity }}" data-product-price="{{ $product->price }}"
              data-product-name="{{ $product->name }}" data-product-type="{{ $product->type }}"
