321f4c85f409bbfa73d8b003e923ed06af8edfd3
CarloColletti~ <carlocolletti5@gmail.com>
2024-04-02 09:58:38 +0200

    create resouce

---
+++ Modules/Orders/Entities/Orders.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Modules\Orders\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Orders extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [];
+}
