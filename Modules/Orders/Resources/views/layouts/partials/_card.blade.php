c151137955496ddd14e86973a1ee90f2e056f595
CarloColletti~ <carlocolletti5@gmail.com>
2024-04-04 13:01:56 +0200

    send list for order_ahopping_cart work

--- Modules/Orders/Resources/views/layouts/partials/_card.blade.php
+++ Modules/Orders/Resources/views/layouts/partials/_card.blade.php
@@ -1,25 +1,19 @@
-{{-- <div class="card card-for-cart" data-id="{{ $product->id }}" style="width: 18rem;">
-    <img src="{{ asset('storage/' . $product->product_image) }}" class="card-img-top img-fluid"
-        alt="product_photo_{{ $product->name }}">
-    <div class="card-body">
-        <h5 class="card-title">{{ $product->name }}</h5>
-    </div>
-    <ul class="list-group list-group-flush">
-        <li class="list-group-item">Prezzo: {{ $product->price }}€</li>
-        <li class="list-group-item">Tipo: {{ $product->type }}</li>
-    </ul>
-</div> --}}
-
-
-
-
 @foreach ($products as $product)
     <div class="col-12 col-sm-6 col-md-4 col-lg-3 g-4 d-flex justify-content-center">
-        <div class="product-card card-for-cart" data-id="{{ $product->id }}" id="product-{{ $product->id }}">
-            <div class="form-check">
-                <input type="checkbox" name="products[]" value="{{ $product->id }}" class="form-check-input">
-                <label for="product-{{ $product->id }}" class="form-check-label">{{ $product->name }}</label>
+        <div class="card card-for-cart" data-id="{{ $product->id }}" id="product-{{ $product->id }}"
+            style="width: 18rem;">
+            <img src="{{ asset('storage/' . $product->product_image) }}" class="card-img-top img-fluid"
+                alt="product_photo_{{ $product->name }}">
+
+            <div class="card-body">
+                <h5 class="card-title">{{ $product->name }}</h5>
             </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Prezzo: {{ $product->price }}€</li>
+                <li class="list-group-item">Tipo: {{ $product->type }}</li>
+                <input type="checkbox" name="products[]" value="{{ $product->id }}" class="form-check-input" default
+                    hidden>
+            </ul>
         </div>
     </div>
 @endforeach
--- public/js/cart.js
+++ public/js/cart.js
@@ -1,33 +1,22 @@
-// Array per memorizzare gli ID dei prodotti selezionati
-const selectedProducts = [];
 
-// Event listener per la selezione dei prodotti
+const productCards = document.querySelectorAll('#card-for-cart');
+
 document.querySelectorAll('.card-for-cart').forEach(card => {
   card.addEventListener('click', () => {
-    
-    const productId = card.getAttribute('data-id');
     const checkbox = card.querySelector('input[type="checkbox"]');
     checkbox.checked = !checkbox.checked;
-
-    // Controllo se il prodotto è già stato selezionato
-    if (selectedProducts.includes(productId)) {
-      selectedProducts.splice(selectedProducts.indexOf(productId),1);
-      
+    if (checkbox.checked) {
+      card.classList.add('border', 'border-success','border-5');
+    } else {
       card.classList.remove('border', 'border-success','border-5');
-      // console.log('****************************');
-      // console.log(selectedProducts);
-      // console.log('****************************');
-
-      return
-    }else{
-      // Aggiunta del prodotto all'array
-      selectedProducts.push(productId);
     }
 
-    // console.log(selectedProducts);
-   
-    card.classList.add('border', 'border-success','border-5');
-
   });
+});
+
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+checkboxes.forEach(checkbox => {
+  checkbox.checked = false;
 });
 
